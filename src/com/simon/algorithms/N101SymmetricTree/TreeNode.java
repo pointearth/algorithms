1f496fc7cb59f505df0338b53e1afe0f2f9d0c40
Simon <cxm@C02XQ12ZJG5H.local>
2019-06-26 16:49:22 +0800

    smmetricTree

---
+++ src/com/simon/algorithms/N101SymmetricTree/TreeNode.java
@@ -0,0 +1,12 @@
+package com.simon.algorithms.N101SymmetricTree;
+
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
